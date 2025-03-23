ad9f6127a9edbf24faf172be4b59533b1863f3b9
Anton Schulte <anton.schulte@hhi.fraunhofer.de>
2025-03-23 15:00:24 +0100

    format

--- main/esp_zb_light.c
+++ main/esp_zb_light.c
@@ -39,39 +39,49 @@ static void bdb_start_top_level_commissioning_cb(uint8_t mode_mask)
 
 void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
 {
-    uint32_t *p_sg_p       = signal_struct->p_app_signal;
+    uint32_t *p_sg_p = signal_struct->p_app_signal;
     esp_err_t err_status = signal_struct->esp_err_status;
     esp_zb_app_signal_type_t sig_type = *p_sg_p;
-    switch (sig_type) {
+    switch (sig_type)
+    {
     case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
         ESP_LOGI(TAG, "Initialize Zigbee stack");
         esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
         break;
     case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
     case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
-        if (err_status == ESP_OK) {
+        if (err_status == ESP_OK)
+        {
             ESP_LOGI(TAG, "Deferred driver initialization %s", deferred_driver_init() ? "failed" : "successful");
             ESP_LOGI(TAG, "Device started up in %s factory-reset mode", esp_zb_bdb_is_factory_new() ? "" : "non");
-            if (esp_zb_bdb_is_factory_new()) {
+            if (esp_zb_bdb_is_factory_new())
+            {
                 ESP_LOGI(TAG, "Start network steering");
                 esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
-            } else {
+            }
+            else
+            {
                 ESP_LOGI(TAG, "Device rebooted");
             }
-        } else {
+        }
+        else
+        {
             /* commissioning failed */
             ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
         }
         break;
     case ESP_ZB_BDB_SIGNAL_STEERING:
-        if (err_status == ESP_OK) {
+        if (err_status == ESP_OK)
+        {
             esp_zb_ieee_addr_t extended_pan_id;
             esp_zb_get_extended_pan_id(extended_pan_id);
             ESP_LOGI(TAG, "Joined network successfully (Extended PAN ID: %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x, PAN ID: 0x%04hx, Channel:%d, Short Address: 0x%04hx)",
                      extended_pan_id[7], extended_pan_id[6], extended_pan_id[5], extended_pan_id[4],
                      extended_pan_id[3], extended_pan_id[2], extended_pan_id[1], extended_pan_id[0],
                      esp_zb_get_pan_id(), esp_zb_get_current_channel(), esp_zb_get_short_address());
-        } else {
+        }
+        else
+        {
             ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
             esp_zb_scheduler_alarm((esp_zb_callback_t)bdb_start_top_level_commissioning_cb, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
         }
@@ -93,9 +103,12 @@ static esp_err_t zb_attribute_handler(const esp_zb_zcl_set_attr_value_message_t 
                         message->info.status);
     ESP_LOGI(TAG, "Received message: endpoint(%d), cluster(0x%x), attribute(0x%x), data size(%d)", message->info.dst_endpoint, message->info.cluster,
              message->attribute.id, message->attribute.data.size);
-    if (message->info.dst_endpoint == HA_ESP_LIGHT_ENDPOINT) {
-        if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF) {
-            if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL) {
+    if (message->info.dst_endpoint == HA_ESP_LIGHT_ENDPOINT)
+    {
+        if (message->info.cluster == ESP_ZB_ZCL_CLUSTER_ID_ON_OFF)
+        {
+            if (message->attribute.id == ESP_ZB_ZCL_ATTR_ON_OFF_ON_OFF_ID && message->attribute.data.type == ESP_ZB_ZCL_ATTR_TYPE_BOOL)
+            {
                 light_state = message->attribute.data.value ? *(bool *)message->attribute.data.value : light_state;
                 ESP_LOGI(TAG, "Light sets to %s", light_state ? "On" : "Off");
                 light_driver_set_power(light_state);
@@ -108,7 +121,8 @@ static esp_err_t zb_attribute_handler(const esp_zb_zcl_set_attr_value_message_t 
 static esp_err_t zb_action_handler(esp_zb_core_action_callback_id_t callback_id, const void *message)
 {
     esp_err_t ret = ESP_OK;
-    switch (callback_id) {
+    switch (callback_id)
+    {
     case ESP_ZB_CORE_SET_ATTR_VALUE_CB_ID:
         ret = zb_attribute_handler((esp_zb_zcl_set_attr_value_message_t *)message);
         break;
@@ -123,7 +137,7 @@ static void esp_zb_task(void *pvParameters)
 {
     /* initialize Zigbee stack */
     esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZED_CONFIG();
-      zcl_basic_manufacturer_info_t info = {
+    zcl_basic_manufacturer_info_t info = {
         .manufacturer_name = ESP_MANUFACTURER_NAME,
         .model_identifier = ESP_MODEL_IDENTIFIER,
     };
@@ -152,8 +166,6 @@ static void esp_zb_task(void *pvParameters)
     esp_zb_ep_list_add_ep(merged_ep_list, esp_zb_on_off_switch_ep, ep_config_switch);
     esp_zcl_utility_add_ep_basic_manufacturer_info(merged_ep_list, HA_ONOFF_SWITCH_ENDPOINT, &info);
 
-  
-
     esp_zb_device_register(merged_ep_list);
     esp_zb_core_action_handler_register(zb_action_handler);
     esp_zb_set_primary_network_channel_set(ESP_ZB_PRIMARY_CHANNEL_MASK);
@@ -164,69 +176,74 @@ static void esp_zb_task(void *pvParameters)
 #define GPIO_PIN_1 11
 bool value = false;
 static QueueHandle_t gpio_evt_queue = NULL;
-void change_state() {
+void change_state()
+{
     ESP_LOGI(TAG, "Turning the switch %s!", value == true ? "ON" : "OFF");
     esp_zb_zcl_on_off_cmd_t cmd_req;
     cmd_req.zcl_basic_cmd.src_endpoint = HA_ONOFF_SWITCH_ENDPOINT;
     cmd_req.address_mode = ESP_ZB_APS_ADDR_MODE_DST_ADDR_ENDP_NOT_PRESENT;
-    if (value) {
+    if (value)
+    {
         cmd_req.on_off_cmd_id = ESP_ZB_ZCL_CMD_ON_OFF_ON_ID;
-    } else {
+    }
+    else
+    {
         cmd_req.on_off_cmd_id = ESP_ZB_ZCL_CMD_ON_OFF_OFF_ID;
-        
     }
     esp_zb_lock_acquire(portMAX_DELAY);
     esp_zb_zcl_on_off_cmd_req(&cmd_req);
     esp_zb_lock_release();
 }
-void toggle_switch(void *arg) {
+void toggle_switch(void *arg)
+{
     uint32_t io_num;
 
-    for (;;) {
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+    for (;;)
+    {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
+        {
             value = true;
             change_state();
-            while (xQueueReceive(gpio_evt_queue, &io_num, pdMS_TO_TICKS(1000))) {
-                
+            while (xQueueReceive(gpio_evt_queue, &io_num, pdMS_TO_TICKS(1000)))
+            {
             }
             value = false;
             change_state();
         }
-       
     }
 }
 
-static void IRAM_ATTR gpio_isr_handler(void* arg)
+static void IRAM_ATTR gpio_isr_handler(void *arg)
 {
-    uint32_t gpio_num = (uint32_t) arg;
+    uint32_t gpio_num = (uint32_t)arg;
     xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
-#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_PIN_0) | (1ULL<<GPIO_PIN_1))
+#define GPIO_INPUT_PIN_SEL ((1ULL << GPIO_PIN_0) | (1ULL << GPIO_PIN_1))
 #define ESP_INTR_FLAG_DEFAULT 0
 
-void setup_gpio(void) {
+void setup_gpio(void)
+{
     gpio_config_t io_conf = {};
 
-    //interrupt of rising edge
+    // interrupt of rising edge
     io_conf.intr_type = GPIO_INTR_POSEDGE;
-    //bit mask of the pins, use GPIO4/5 here
+    // bit mask of the pins, use GPIO4/5 here
     io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
-    //set as input mode
+    // set as input mode
     io_conf.mode = GPIO_MODE_INPUT;
-    //enable pull-up mode
+    // enable pull-up mode
     io_conf.pull_up_en = 0;
     gpio_config(&io_conf);
-    //change gpio interrupt type for one pin
+    // change gpio interrupt type for one pin
     gpio_set_intr_type(GPIO_PIN_0, GPIO_INTR_ANYEDGE);
     gpio_set_intr_type(GPIO_PIN_1, GPIO_INTR_ANYEDGE);
-    //create a queue to handle gpio event from isr
-    gpio_evt_queue = xQueueCreate(10,  sizeof(uint32_t));
-    //install gpio isr service
+    // create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    // install gpio isr service
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(GPIO_PIN_0, gpio_isr_handler, (void*) GPIO_PIN_0);
-    gpio_isr_handler_add(GPIO_PIN_1, gpio_isr_handler, (void*) GPIO_PIN_1);
-
+    // hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_PIN_0, gpio_isr_handler, (void *)GPIO_PIN_0);
+    gpio_isr_handler_add(GPIO_PIN_1, gpio_isr_handler, (void *)GPIO_PIN_1);
 }
 void app_main(void)
 {
